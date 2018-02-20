dc2390e63cd857d86cba6cf8f79351ecf158e08a
Adam Boyajian <adam.boyajian@wework.com>
2018-02-20 03:13:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,11 +36,7 @@ end
 
 def full?(board)
   board.each do |status|
-    if status[0] == "X" or "O" &&
-       status[1] == "X" or "O" &&
-       status[2] == "X" or "O" &&
-       status[3] == "X" or "O" &&
-       status[4] == "X" or "O" &&
+    if status[0] == "X" or "O" && status[1] == "X" or "O" && status[2] == "X" or "O" && status[3] == "X" or "O" && status[4] == "X" or "O" &&
        status[5] == "X" or "O" &&
        status[6] == "X" or "O" &&
        status[7] == "X" or "O" &&
